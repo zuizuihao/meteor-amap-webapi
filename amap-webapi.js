98a3c4eeff223cb481ac25fac04334cec9acfda5
zuizuihao <1165622982@qq.com>
2016-07-05 18:26:57 +0800

    modify static map method.

--- amap-webapi.js
+++ amap-webapi.js
@@ -1,28 +1,28 @@
-var setting = Meteor.settings.public.amap;
+var setting = Meteor.settings.public.amap
 if (!setting) {
-  console.log('error', 'Please Add amap setting.');
+  console.log('error', 'Please Add amap setting.')
 }
-const host = 'http://restapi.amap.com/v3/';
+const host = 'http://restapi.amap.com/v3/'
 
-AMapWebAPI = {};
+AMapWebAPI = {}
 
-AMapWebAPI.getDistance = function getDistance(start, end, callback) {
+AMapWebAPI.getDistance = function getDistance (start, end, callback) {
   var queryString = {
     origins: start,
     destination: end,
     output: 'json',
     key: setting.webapikey
-  };
+  }
 
   var response = HTTP.get(host + 'distance', { params: queryString }, function (error, result) {
-    var distance = -1;
-    var results = JSON.parse(result.content).results;
+    var distance = -1
+    var results = JSON.parse(result.content).results
     if (results && results.length > 0) {
-      distance = results[0].distance;
+      distance = results[0].distance
     }
 
-    callback(error, distance);
-  });
+    callback(error, distance)
+  })
 }
 
 AMapWebAPI.regeocode = function (location, callback) {
@@ -32,11 +32,11 @@ AMapWebAPI.regeocode = function (location, callback) {
     radius: 1000,
     extensions: 'base',
     key: setting.webapikey
-  };
+  }
 
   HTTP.get(host + 'geocode/regeo', { params: queryString }, function (error, result) {
-    callback(error, result.data.regeocode);
-  });
+    callback(error, result.data.regeocode)
+  })
 }
 
 AMapWebAPI.geocode = function (address, callback) {
@@ -44,11 +44,11 @@ AMapWebAPI.geocode = function (address, callback) {
     address: address,
     output: 'json',
     key: setting.webapikey
-  };
+  }
 
   HTTP.get(host + 'geocode/geo', { params: queryString }, function (error, result) {
-    callback(error, result.data.geocodes[0].location);
-  });
+    callback(error, result.data.geocodes[0].location)
+  })
 }
 
 AMapWebAPI.getStaticMap = function (locations, callback) {
@@ -57,17 +57,15 @@ AMapWebAPI.getStaticMap = function (locations, callback) {
     paths: '5,0x0000FF,1,,:' + getLocationsString(locations),
     markers: getMarkersString(locations, '起', '现'),
     key: setting.webapikey
-  };
+  }
 
-  HTTP.get(host + 'staticmap?' + ConvertToQueryString(query), function (error, result) {
-    callback(error, result.content);
-  });
+  return host + 'staticmap?' + ConvertToQueryString(query)
 }
 
 AMapWebAPI.getCurrentPosition = function (callback) {
   navigator.geolocation.getCurrentPosition(callback, function () {
-    console.log('No access to get position');
-  });
+    console.log('No access to get position')
+  })
 }
 
 AMapWebAPI.getCurrentAddress = function (callback) {
@@ -75,57 +73,57 @@ AMapWebAPI.getCurrentAddress = function (callback) {
     var coordinates = {
       lat: position.coords.latitude,
       lng: position.coords.longitude
-    };
+    }
     AMapWebAPI.regeocode(coordinates, function (error, result) {
       if (callback) {
-        console.log(result);
+        console.log(result)
         callback(error, {
           coordinates: coordinates,
           address: result.addressComponent.province + ' ' + result.addressComponent.city + ' ' + result.addressComponent.district,
           addressComponent: result.addressComponent
-        });
+        })
       }
-    });
-  });
+    })
+  })
 }
 
-function getLocationsString(locations) {
-  var ret = '';
+function getLocationsString (locations) {
+  var ret = ''
   for (var index = 0; index < locations.length - 1; index++) {
-    ret += getPositionString(locations[index]) + ';';
+    ret += getPositionString(locations[index]) + ';'
   }
-  ret += getPositionString(locations[locations.length - 1]);
-  return ret;
+  ret += getPositionString(locations[locations.length - 1])
+  return ret
 }
 
-function getMarkersString(locations, startText, endText) {
-  var startStr = 'mid,,' + startText + ':' + getPositionString(locations[0]);
-  var endStr = '|mid,,' + endText + ':' + getPositionString(locations[locations.length - 1]);
-  locations.shift();
-  locations.pop();
-  var midStr = '';
+function getMarkersString (locations, startText, endText) {
+  var startStr = 'mid,,' + startText + ':' + getPositionString(locations[0])
+  var endStr = '|mid,,' + endText + ':' + getPositionString(locations[locations.length - 1])
+  locations.shift()
+  locations.pop()
+  var midStr = ''
   if (locations.length > 0)
-    midStr = '|small,,:' + getLocationsString(locations);
-  return startStr + midStr + endStr;
+    midStr = '|small,,:' + getLocationsString(locations)
+  return startStr + midStr + endStr
 }
 
-function getPositionString(coordinates) {
-  return coordinates.lng + ',' + coordinates.lat;
+function getPositionString (coordinates) {
+  return coordinates.lng + ',' + coordinates.lat
 }
 
-function getPositionsString(coordinatesList) {
-  var result = getPositionString(coordinatesList[0]);
+function getPositionsString (coordinatesList) {
+  var result = getPositionString(coordinatesList[0])
   for (var index = 1; index < coordinatesList.length; index++) {
-    result += '|' + getPositionString(coordinatesList[index]);
+    result += '|' + getPositionString(coordinatesList[index])
   }
-  return result;
+  return result
 }
 
-function ConvertToQueryString(obj) {
-  var str = [];
+function ConvertToQueryString (obj) {
+  var str = []
   for (var p in obj)
     if (obj.hasOwnProperty(p)) {
-      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-    }
-  return str.join("&");
+      str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]))
+  }
+  return str.join('&')
 }
